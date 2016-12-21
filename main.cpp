0dbffce7af80ef97bf14e6fb6a871872088e962b
BrainStone <yannick@tedworld.de>
2016-12-21 19:57:06 +0100

    Fixed main

--- main.cpp
+++ main.cpp
@@ -15,9 +15,8 @@ int main( int argc, char* argv[] ) {
 	po::options_description desc( "Allowed options" );
 	desc.add_options()
 		("help,h", "Displays this help page")
+		("debug,d", "Use debug logger format")
 		("verbose,v", po::value<int>( &verbosity )->default_value( 0 )->implicit_value( 9 ), "Sets the verbosity level (0-9). Sets the level to 9 if no level is specified. Verbosity level is 0 if this option is not specified")
-		("port,p", po::value<short>( &port ), "Sets the port. Default is the config value")
-		("ip,i", po::value<string>( &ip ), "Sets the address tio listen to. Default is the config value")
 		;
 
 	po::variables_map vm;

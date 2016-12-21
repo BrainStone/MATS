32d3dab6132e9ec53135f77df374d7dc9c916b20
BrainStone <yannick@tedworld.de>
2016-12-21 21:07:04 +0100

    More logging settings

--- Logger.h
+++ Logger.h
@@ -24,20 +24,22 @@ inline void initLogger( bool debug, int verbosity ) {
 
 	defaultConf.setGlobally( el::ConfigurationType::Format, formatDefault );
 	defaultConf.set( el::Level::Verbose, el::ConfigurationType::Format, formatVerbose );
-	defaultConf.set( el::Level::Debug, el::ConfigurationType::Format, formatDetailed );
 	defaultConf.set( el::Level::Warning, el::ConfigurationType::Format, formatDetailed );
 	defaultConf.set( el::Level::Error, el::ConfigurationType::Format, formatDetailed );
 	defaultConf.set( el::Level::Fatal, el::ConfigurationType::Format, formatDetailed );
-
 	defaultConf.setGlobally( el::ConfigurationType::Filename, "log/log.log" );
-
 	defaultConf.setGlobally( el::ConfigurationType::Enabled, "true" );
-	defaultConf.set( el::Level::Trace, el::ConfigurationType::Enabled, debug ? "true" : "false" );
 	defaultConf.setGlobally( el::ConfigurationType::ToFile, "true" );
 	defaultConf.setGlobally( el::ConfigurationType::ToStandardOutput, "false" );
 	defaultConf.setGlobally( el::ConfigurationType::MillisecondsWidth, debug ? "6" : "3" );
 	defaultConf.setGlobally( el::ConfigurationType::PerformanceTracking, debug ? "true" : "false" );
 	defaultConf.setGlobally( el::ConfigurationType::MaxLogFileSize, debug ? "1073741824" : "134217728" );
+
+	if ( debug ) {
+		defaultConf.set( el::Level::Debug, el::ConfigurationType::Format, formatDetailed );
+	} else {
+		defaultConf.set( el::Level::Trace, el::ConfigurationType::Enabled, "false" );
+	}
 
 	// Set options
 	el::Loggers::reconfigureLogger( "default", defaultConf );

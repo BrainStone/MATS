f20661f8694d417309f66f2917ad70a885863c6a
BrainStone <yannick@tedworld.de>
2016-12-29 21:31:16 +0100

    Further script improvements

--- install.sh
+++ install.sh
@@ -1,21 +1,21 @@
 #! /bin/bash
 
 # Make sure the user did not forget to update the submodules and they are at the correct version
-echo -e "\e[1mCloning all submodules\e[m"
+echo -e "\e[1mCloning all submodules\n----------------------\e[m"
 git submodule update --init --recursive
 git submodule sync --recursive
 
 if [ $# -eq 0 ] || [ "_$1" != "_-s" ]
 then
   # Install dependencies
-  echo -e "\e[1mInstalling all dependencies\e[m"
+  echo -e "\n\e[1mInstalling all required dependencies\n------------------------------------\e[m"
   sudo apt-get install -y g++ libboost-all-dev libconfig++-dev libncursesw5-dev
 fi
 
 # Make and install the program
-echo -e "\e[1mCompiling...\e[m"
+echo -e "\n\e[1mCompiling\n---------\e[m"
 make release
-echo -e "\e[1mInstalling...\e[m"
+echo -e "\n\e[1mInstalling\n----------\e[m"
 sudo make install
 
-echo -e "\n\e[1mDone!\e[m\n"
+echo -e "\n\n\e[1mDone!\n=====\e[m\n"
